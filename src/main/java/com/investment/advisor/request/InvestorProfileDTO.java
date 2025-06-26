0f4bbb83a47297a91b206c2c826405157581241a
Ashika3115 <ashikajinturkar@gmail.com>
2025-06-26 21:40:44 +0530

    feat:Using record functionalty from Java 17

--- src/main/java/com/investment/advisor/request/InvestorProfileDTO.java
+++ src/main/java/com/investment/advisor/request/InvestorProfileDTO.java
@@ -1,22 +1,14 @@
 package com.investment.advisor.request;
 
-import lombok.*;
-
 import java.util.List;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class InvestorProfileDTO {
-    private Long id;
-    private String name;
-    private Integer age;
-    private Double income;
-    private Double savings;
-    private String riskTolerance;
-    private List<String> investmentGoal;
-    private String createdAt;
-
-}
+public record InvestorProfileDTO(
+    Long id,
+    String name,
+    Integer age,
+    Double income,
+    Double savings,
+    String riskTolerance,
+    List<String> investmentGoal,
+    String createdAt
+) {}
