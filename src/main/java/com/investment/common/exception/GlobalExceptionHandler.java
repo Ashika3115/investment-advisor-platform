2ad7303aae7eeb954cedfabd31c9a99543ee91cc
Ashika3115 <ashikajinturkar@gmail.com>
2025-06-26 22:33:33 +0530

    feat:Adding method for JPA exception in GlobalExceptionHandler.java

--- src/main/java/com/investment/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/investment/common/exception/GlobalExceptionHandler.java
@@ -2,7 +2,11 @@ package com.investment.common.exception;
 
 import com.investment.common.apiresponse.ApiResponse;
 import jakarta.persistence.EntityNotFoundException;
+import org.postgresql.util.PSQLException;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.*;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
+import org.springframework.transaction.CannotCreateTransactionException;
 import org.springframework.web.bind.annotation.*;
 
 @RestControllerAdvice
@@ -26,6 +30,47 @@ public class GlobalExceptionHandler {
                         .message(ex.getMessage())
                         .data(null)
                         .build());
+    }
+
+    @ExceptionHandler(CannotGetJdbcConnectionException.class)
+    public ApiResponse<?> handleDatabaseConnectionError(CannotGetJdbcConnectionException ex) {
+        return ApiResponse.builder()
+                .status("error")
+                .message("Database connection failed. Please try again later.")
+                .data(null)
+                .build();
+    }
+
+    @ExceptionHandler(DataAccessException.class)
+    public ApiResponse<?> handleDataAccessException(DataAccessException ex) {
+        return ApiResponse.builder()
+                .status("error")
+                .message("Database error occurred.")
+                .data(null)
+                .build();
+    }
+
+
+    @ExceptionHandler(CannotCreateTransactionException.class)
+    public ResponseEntity<ApiResponse<?>> handleCannotCreateTransactionException(CannotCreateTransactionException ex) {
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(
+                ApiResponse.builder()
+                        .status("error")
+                        .message("Cannot connect to the database. Please try again later.")
+                        .data(null)
+                        .build()
+        );
+    }
+
+    @ExceptionHandler(PSQLException.class)
+    public ResponseEntity<ApiResponse<?>> handleCannotCreateTransactionException(PSQLException ex) {
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(
+                ApiResponse.builder()
+                        .status("error")
+                        .message("The connection attempt failed. Please try again later.")
+                        .data(null)
+                        .build()
+        );
     }
 
     @ExceptionHandler(Exception.class)
